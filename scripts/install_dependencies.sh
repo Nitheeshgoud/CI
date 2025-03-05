20f41d0ca2610f3f0d0d585069d69f1dc395cf63
Nitheeshgoud <141002004+Nitheeshgoud@users.noreply.github.com>
2025-03-05 17:41:49 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Create the application directory if it doesn't exist
+mkdir -p /home/ubuntu/app
+
+# Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 # Install Node.js dependencies
 cd /home/ubuntu/app
 npm install
