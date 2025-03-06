65bd1bc7b71acce95d2636025716fcdcac7e5718
Nitheeshgoud <141002004+Nitheeshgoud@users.noreply.github.com>
2025-03-06 15:32:30 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-set -e  # Exit on error
-
+# Clean up the application directory
 echo "Cleaning up /home/ubuntu/app..."
-
-# Change ownership to ubuntu user (if files were created by root)
-sudo chown -R ubuntu:ubuntu /home/ubuntu/app
-
-# Grant full permissions to the user
-sudo chmod -R u+rwx /home/ubuntu/app
-
-# Remove files forcefully
-rm -rf /home/ubuntu/app/*
-
+rm -rf /home/ubuntu/app/public/css/*
+rm -rf /home/ubuntu/app/public/js/*
+rm -rf /home/ubuntu/app/public/img/*
+rm -rf /home/ubuntu/app/public/*.html
+rm -rf /home/ubuntu/app/scripts/*
 echo "Cleanup complete."
