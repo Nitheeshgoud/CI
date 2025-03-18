9112f6189ccd15a224fbb4084772764e89094c0a
Nitheeshgoud <141002004+Nitheeshgoud@users.noreply.github.com>
2025-03-18 18:20:47 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Start the Node.js application
-cd /home/ubuntu/app
-npm start
+
+echo "Starting the Node.js application..."
+cd /home/ubuntu/app || { echo "Failed to navigate to /home/ubuntu/app"; exit 1; }
+
+nohup node server.js > /dev/null 2>&1 &
+
+echo "Application started successfully."
