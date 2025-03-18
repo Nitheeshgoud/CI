f2cc7a87157363c2340b84c888bcb3091e598b43
Nitheeshgoud <141002004+Nitheeshgoud@users.noreply.github.com>
2025-03-18 18:19:43 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Clean up the application directory
-echo "Cleaning up /home/ubuntu/app..."
+
+echo "Cleaning up old application files..."
 rm -rf /home/ubuntu/app/public/css/*
 rm -rf /home/ubuntu/app/public/js/*
 rm -rf /home/ubuntu/app/public/img/*
 rm -rf /home/ubuntu/app/public/*.html
 rm -rf /home/ubuntu/app/scripts/*
+
 echo "Cleanup complete."
