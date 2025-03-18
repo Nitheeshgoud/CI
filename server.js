bb12afbe2e711bb028ccc9d6b3a7b13bfed661ec
Nitheeshgoud <141002004+Nitheeshgoud@users.noreply.github.com>
2025-03-18 18:18:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,48 +2,48 @@ const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
 
-let initial_path = path.join(__dirname, "public");
-
 const app = express();
+const initial_path = path.join(__dirname, "public");
+
 app.use(express.static(initial_path));
 app.use(fileupload());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "home.html"));
-})
+});
 
 app.get('/editor', (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"));
-})
+});
 
-// upload link
+// Upload route
 app.post('/upload', (req, res) => {
+    if (!req.files || !req.files.image) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
     let file = req.files.image;
     let date = new Date();
-    // image name
-    let imagename = date.getDate() + date.getTime() + file.name;
-    // image upload path
-    let path = 'public/uploads/' + imagename;
+    let imagename = `${date.getDate()}${date.getTime()}_${file.name}`;
+    let uploadPath = path.join(__dirname, "public/uploads", imagename);
 
-    // create upload
-    file.mv(path, (err, result) => {
-        if(err){
-            throw err;
-        } else{
-            // our image upload path
-            res.json(`uploads/${imagename}`)
+    file.mv(uploadPath, (err) => {
+        if (err) {
+            return res.status(500).send(err);
         }
-    })
-})
+        res.json(`uploads/${imagename}`);
+    });
+});
 
 app.get("/:blog", (req, res) => {
     res.sendFile(path.join(initial_path, "blog.html"));
-})
+});
 
+// 404 Handling
 app.use((req, res) => {
-    res.json("404");
-})
+    res.status(404).json({ error: "404 - Page Not Found" });
+});
 
-app.listen("80", () => {
-    console.log('listening......');
-})
+app.listen(80, () => {
+    console.log('Server is running on port 80...');
+});
