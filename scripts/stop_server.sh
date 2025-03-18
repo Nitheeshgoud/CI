489b423c9d3d625fff70552b1bd29b45e745897b
Nitheeshgoud <141002004+Nitheeshgoud@users.noreply.github.com>
2025-03-18 23:09:47 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-echo "Stopping the Node.js application..."
-pkill -f "node server.js" && echo "Application stopped." || echo "No running application found."
+echo "Stopping application..."
+pm2 stop all || true
